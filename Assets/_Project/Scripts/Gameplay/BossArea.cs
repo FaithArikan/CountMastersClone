98e35431c44664ad81e2d6df5deebba3f8c0cc65
FaithArikan <fatih@pow.games>
2022-08-26 22:35:55 +0300

    feat: Add BossArea

---
+++ Assets/_Project/Scripts/Gameplay/BossArea.cs
@@ -0,0 +1,34 @@
+﻿using _Project.Scripts.Helpers;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class BossArea : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onBossFightStarted;
+        private Sequence _moveSequence;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.transform.DOMove(new Vector3(transform.position.x, 0, transform.position.z), 1);
+                onBossFightStarted.Invoke();
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                _moveSequence.Append(other.transform.DOMove(new Vector3(transform.position.x, 0, 
+                    transform.position.z), 0.5f));
+            }
+        }
+        public void CloneEndState()
+        {
+            DOTween.Kill(_moveSequence);
+        }
+    }
+}
