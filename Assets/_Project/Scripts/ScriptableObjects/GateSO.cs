c998edbff25e762342540efc5d00aa1d9ea35d64
FaithArikan <fatih@pow.games>
2022-08-26 22:39:34 +0300

    feat: Add GateSO

---
+++ Assets/_Project/Scripts/ScriptableObjects/GateSO.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Gate", order = 1)]
+    public class GateSO : ScriptableObject
+    {
+        [Header("LeftGate")]
+        [SerializeField] private GateFeatures gateFeatures;
+        [SerializeField] private int amount;
+        public GateFeatures Features
+        {
+            get => gateFeatures;
+            set => gateFeatures = value;
+        }
+
+        public int Amount
+        {
+            get => amount;
+            set => amount = value;
+        }
+    }
+}
