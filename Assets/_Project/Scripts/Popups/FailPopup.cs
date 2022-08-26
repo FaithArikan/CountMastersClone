eaeb2f1c431f958d529dc9f8a6bc67f9d0e34814
FaithArikan <fatih@pow.games>
2022-08-26 22:39:11 +0300

    feat: Add FailPopup

---
+++ Assets/_Project/Scripts/Popups/FailPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Popups
+{
+    public class FailPopup : MonoBehaviour
+    {
+        public void ReloadScene()
+        {
+            string gameScene = "ExampleGameScene";
+            SceneManager.UnloadSceneAsync(gameScene);
+            SceneManager.LoadScene(gameScene, LoadSceneMode.Additive);
+        }
+    }
+}
