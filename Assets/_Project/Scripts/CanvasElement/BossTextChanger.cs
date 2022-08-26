306474e98e4209e567b823f60fc0c477e0f678aa
FaithArikan <fatih@pow.games>
2022-08-26 22:35:26 +0300

    feat: Add BossTextChanger

---
+++ Assets/_Project/Scripts/CanvasElement/BossTextChanger.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay.CanvasElement
+{
+    public class BossTextChanger : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI bossPowerText;
+        [SerializeField] private BossController bossController;
+
+        private void Start()
+        {
+            bossPowerText.text = bossController.BossPower.ToString();
+        }
+
+        public void OnBossPowerAmountChanged()
+        {
+            bossPowerText.text = bossController.BossPower.ToString();
+        }
+
+    }
+}
