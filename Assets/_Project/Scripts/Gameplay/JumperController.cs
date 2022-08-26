000d4a2f34d31bcd55563d15a38a3f44587069f5
FaithArikan <fatih@pow.games>
2022-08-26 22:37:09 +0300

    feat: Add JumperController

---
+++ Assets/_Project/Scripts/Gameplay/JumperController.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class JumperController : MonoBehaviour
+    {
+        [SerializeField] private Transform jumpEndPoint;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.transform.DOJump(jumpEndPoint.position, 1f, 1, 1);
+            }
+        }
+
+    }
+}
