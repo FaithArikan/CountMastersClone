906cff450f094cca37ca4b880a0089bc4768b6bd
FaithArikan <fatih@pow.games>
2022-08-26 22:37:02 +0300

    feat: Add ObstacleController

---
+++ Assets/_Project/Scripts/Gameplay/HammerObstacleController.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class HammerObstacleController : MonoBehaviour
+    {
+        [SerializeField] private bool isLeftSideHammer;
+        private int _hammerAngle;
+        [SerializeField] private int hammerSpeed = 1;
+        private void Start()
+        {
+            if (isLeftSideHammer)
+                _hammerAngle = -90;
+            else
+                _hammerAngle = 90;
+            Move();
+        }
+        private void Move()
+        {
+            transform.DORotate(new Vector3(0, 0, _hammerAngle), hammerSpeed).SetLoops(-1, LoopType.Yoyo).
+                SetEase(Ease.Linear);
+        }
+    }
+}
