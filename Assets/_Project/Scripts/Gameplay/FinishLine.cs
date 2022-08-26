fe38a60cc8766d41b4df21c3ddfd8774110b42a5
FaithArikan <fatih@pow.games>
2022-08-26 22:36:41 +0300

    feat: Add FinishLine

---
+++ Assets/_Project/Scripts/Gameplay/FinishLine.cs
@@ -0,0 +1,17 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class FinishLine : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onBuildTower;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                onBuildTower.Invoke();
+            }
+        }
+    }
+}
