309d2bae8925e5426e6173f724a66e5a24f21a2f
FaithArikan <fatih@pow.games>
2022-08-26 22:36:03 +0300

    feat: Add BossController

---
+++ Assets/_Project/Scripts/Gameplay/BossController.cs
@@ -0,0 +1,35 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class BossController : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onLevelFinished;
+        [SerializeField] private GameEvent onBossPowerChanged;
+        [SerializeField] private int bossPower;
+
+        public int BossPower
+        {
+            get => bossPower;
+            set
+            {
+                bossPower = value;
+                onBossPowerChanged.Invoke();
+                if (bossPower <= 0)
+                {
+                    Destroy(gameObject);
+                    onLevelFinished.Invoke();
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Clone"))
+            {
+                BossPower--;
+            }
+        }
+    }
+}
