5fc9c7020f73d47c2816a848858eeb15800b804e
FaithArikan <fatih@pow.games>
2022-08-26 22:37:55 +0300

    feat: Add GameEventListener

---
+++ Assets/_Project/Scripts/Helpers/GameEventListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Project.Scripts.Helpers
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent[] Events;
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            foreach (var e in Events) e.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            foreach (var e in Events) e.UnregisterListener(this);
+
+        }
+
+        public void Invoke()
+        {
+            Response.Invoke();
+        }
+    }
+
+}
