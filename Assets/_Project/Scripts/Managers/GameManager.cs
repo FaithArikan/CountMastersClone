c29d1fc2cbcc25f94f5db4b2c86213ffaf580691
FaithArikan <fatih@pow.games>
2022-08-26 22:38:17 +0300

    feat: Add GameManager

---
+++ Assets/_Project/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.Scripts.Managers
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        public void OnLose()
+        {
+            Debug.Log("Game Lost");
+        }
+
+        public void OnWin()
+        {
+            Debug.Log("Game Won");
+        }
+    }
+}
