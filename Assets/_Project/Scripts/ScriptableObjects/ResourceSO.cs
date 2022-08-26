1d814c55e6e4cbd75b39a0795168d4f7065cdac7
FaithArikan <fatih@pow.games>
2022-08-26 22:39:42 +0300

    feat: Add ResourceSO

---
+++ Assets/_Project/Scripts/ScriptableObjects/ResourceSO.cs
@@ -0,0 +1,44 @@
+using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Resource", order = 0)]
+    public class ResourceSO : ScriptableObject
+    {
+        [SerializeField] private ResourceType resourceType;
+        [SerializeField] private int maximum = int.MaxValue;
+        [SerializeField] private int minimum;
+        [SerializeField] private int amount;
+        [SerializeField] private GameEvent onResourceValueChanged;
+        public int Maximum => maximum;
+
+        public int Amount
+        {
+            get => amount;
+            set
+            {
+                amount = value;
+                onResourceValueChanged.Invoke();
+            }
+        }
+        
+        public void Increase(int increaseAmount)
+        {
+            if (amount >= maximum) return;
+            increaseAmount = Mathf.Clamp(increaseAmount, minimum, maximum);
+            Amount += increaseAmount;
+        }
+        
+        public void Decrease(int count = 1)
+        {
+            if (amount <= minimum) return;
+            Amount -= count;
+        }
+
+        public void ResetValue()
+        {
+            Amount = minimum;
+        }
+    }
+}
