e574c53ca1e1d32c1aeb71853d779e577a367645
FaithArikan <fatih@pow.games>
2022-08-26 22:38:32 +0300

    feat: Add SceneController

---
+++ Assets/_Project/Scripts/Managers/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Managers
+{
+    public class SceneController : Singleton<SceneController>
+    {
+        private void Start()
+        {
+            LoadBeginScenesAdditive();
+        }
+
+        private void LoadBeginScenesAdditive()
+        {
+            SceneManager.LoadScene("ExampleGameScene", LoadSceneMode.Additive);
+        }
+    }
+}
