c3162f00cb8080977b09b16d4cc4132df62dc10a
FaithArikan <fatih@pow.games>
2022-08-26 22:36:09 +0300

    feat: Add BridgeButton

---
+++ Assets/_Project/Scripts/Gameplay/BridgeButton.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.Helpers;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class BridgeButton : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onBridgeButtonPressed;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Clone"))
+            {
+                onBridgeButtonPressed.Invoke();
+                transform.DOMoveY(-0.5f, 0.5f);
+            }
+        }
+    }
+}
