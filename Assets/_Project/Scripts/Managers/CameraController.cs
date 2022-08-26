34f924384f2105ccd00a367633a724728dd3625d
FaithArikan <fatih@pow.games>
2022-08-26 22:38:09 +0300

    feat: Add CameraController

---
+++ Assets/_Project/Scripts/Managers/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Managers
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+        [SerializeField] private Transform cam;
+        private Vector3 offset;
+
+        private void Start()
+        {
+            offset = cam.position - target.position;
+        }
+        void LateUpdate()
+        {
+            cam.position = target.position + offset;
+        }
+    }
+}
