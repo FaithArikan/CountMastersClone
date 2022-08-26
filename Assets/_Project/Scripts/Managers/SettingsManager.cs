fa920d9fa5aa17fd3ea437bea3326953738f8f24
FaithArikan <fatih@pow.games>
2022-08-26 22:38:46 +0300

    feat: Add SetttingsManager

---
+++ Assets/_Project/Scripts/Managers/SettingsManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts.Managers
+{
+    public class SettingsManager : MonoBehaviour
+    {
+        [SerializeField] private string linkedInURL;
+        [SerializeField] private string githubURL;
+        public void OpenLinkedInProfile()
+        {
+            Application.OpenURL($"{linkedInURL}");
+        }
+        public void OpenGithubProfile()
+        {
+            Application.OpenURL($"{githubURL}");
+        }
+    }
+}
