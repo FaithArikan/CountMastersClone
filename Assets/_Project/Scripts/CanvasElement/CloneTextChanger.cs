e5dc3308754bc5bfb998bb1a9e5a86bc9f060e59
FaithArikan <fatih@pow.games>
2022-08-26 22:35:34 +0300

    feat: Add CloneTextChanger

---
+++ Assets/_Project/Scripts/CanvasElement/CloneTextChanger.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay.CanvasElement
+{
+    public class CloneTextChanger : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI cloneText;
+        [SerializeField] private Player.Player player;
+        
+        public void OnCloneAmountChanged()
+        {
+            cloneText.text = player.CloneAmount.ToString();
+        }
+
+    }
+}
