2875546d0ef8554561087f479fa9cadceeff81e1
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-02 16:05:44 +0300

    feat: Add IntVariable

---
+++ Assets/_Project/Scripts/Variables/IntVariable.cs
@@ -0,0 +1,22 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.Variables
+{
+    [CreateAssetMenu(menuName = "Int")]
+    public class IntVariable : ScriptableObject
+    {
+        [SerializeField] private int intValue;
+        [SerializeField] private GameEvent onValueChanged;
+
+        public int Value
+        {
+            get => intValue;
+            set
+            {
+                intValue = value;
+                if (onValueChanged != null) onValueChanged.Invoke();
+            }
+        }
+    }
+}
