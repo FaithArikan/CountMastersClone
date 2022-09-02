6163dfec39c4d3c2a8b7a9a6d7212c44169dd1d7
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-02 16:05:22 +0300

    feat: Add BooleanVariable

---
+++ Assets/_Project/Scripts/Variables/BooleanVariable.cs
@@ -0,0 +1,23 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.Variables
+{
+    [CreateAssetMenu(menuName = "Boolean")]
+    public class BooleanVariable : ScriptableObject
+    {
+        [SerializeField] private bool boolValue;
+        [SerializeField] private GameEvent onValueChanged;
+
+        public bool Value
+        {
+            get => boolValue;
+            set
+            {
+                boolValue = value;
+                if(onValueChanged != null) onValueChanged.Invoke();
+            }
+        }
+
+    }
+}
