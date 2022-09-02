df0c515143da577171a43cc5be0cf9a415bcfd1c
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-02 16:05:37 +0300

    feat: Add FloatVariable

---
+++ Assets/_Project/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,22 @@
+﻿using _Project.Scripts.Helpers;
+using UnityEngine;
+
+namespace _Project.Variables
+{
+    [CreateAssetMenu(menuName = "Float")]
+    public class FloatVariable : ScriptableObject
+    {
+        [SerializeField] private float floatValue;
+        [SerializeField] private GameEvent onValueChanged;
+
+        public float Value
+        {
+            get => floatValue;
+            set
+            {
+                floatValue = value;
+                if (onValueChanged != null) onValueChanged.Invoke();
+            }
+        }
+    }
+}
