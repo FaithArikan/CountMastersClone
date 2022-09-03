0b880b4caf76f708ad94c84ef313637ec02f24ab
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-03 17:04:17 +0300

    feat: Add CloneSO ScriptableObject Class

---
+++ Assets/_Project/Scripts/ScriptableObjects/CloneSO.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using CountMasters.Gameplay;
+using CountMasters.Helpers;
+using UnityEngine;
+
+namespace CountMasters.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "CloneSO")]
+    public class CloneSO : ScriptableObject
+    {
+         [SerializeField] private List<CloneDestroy> clonerList = new();
+         [SerializeField] private GameEvent onTotalCloneAmountChanged;
+         [SerializeField] private GameEvent onLoseEvent;
+
+         public List<CloneDestroy> Value
+         {
+             get => clonerList;
+             set
+             {
+                 clonerList = value;
+                 InvokeOnValueChanged();
+             }
+         }
+         public int CloneAmount => clonerList.Count;
+
+         public void InitializeList()
+         {
+             clonerList = new ();
+         }
+
+         public void AddClone(CloneDestroy cloneDestroy)
+         {
+             if (!clonerList.Contains(cloneDestroy))
+             {
+                 clonerList.Add(cloneDestroy);
+                 InvokeOnValueChanged();
+             }
+         }
+         
+         public void RemoveClone(CloneDestroy cloneDestroy)
+         {
+             if (clonerList.Contains(cloneDestroy))
+             {
+                 clonerList.Remove(cloneDestroy);
+                 InvokeOnValueChanged();
+             }
+         }
+         private void InvokeOnValueChanged()
+         {
+             if (onTotalCloneAmountChanged != null)
+             {
+                 onTotalCloneAmountChanged.Invoke();
+                 if(CloneAmount > 0) return;
+                 onLoseEvent.Invoke();
+             }
+         }
+    }
+}
