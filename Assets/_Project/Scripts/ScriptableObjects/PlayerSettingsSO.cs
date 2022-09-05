99bd82e633370659569f9563e12e37e61827a60e
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-06 02:17:27 +0300

    feat: Add PlayerSettingsSO script

---
+++ Assets/_Project/Scripts/ScriptableObjects/PlayerSettingsSO.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace CountMasters.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Core/PlayerSettings", fileName = "PlayerSettings")]
+    public class PlayerSettingsSO : ScriptableObject
+    {
+        [SerializeField] private bool canMove;
+        [SerializeField] private float speed;
+        [SerializeField] private float sideSpeed;
+
+        public bool CanMove
+        {
+            get => canMove;
+            set => canMove = value;
+        }
+
+        public float Speed
+        {
+            get => speed;
+            set => speed = value;
+        }
+
+        public float SideSpeed
+        {
+            get => sideSpeed;
+            set => sideSpeed = value;
+        }
+    }
+}
