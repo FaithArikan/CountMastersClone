317178862e36b3fac91ac392597a5db3f9cf6e9b
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-05 21:36:02 +0300

    feat: Add level script

---
+++ Assets/_Project/Scripts/LevelManagement/Level.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CountMasters.LevelManagement
+{
+    public class Level : MonoBehaviour
+    {
+        [SerializeField] private bool _isLevelDone;
+        public bool IsLevelDone
+        {
+            get => _isLevelDone;
+            set => _isLevelDone = value;
+        }
+    }
+}
