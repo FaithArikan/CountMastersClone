b7022cd928ce916d258dc7d4b9c11421416234bc
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-05 21:27:21 +0300

    feat: Add Level prefabs

---
+++ Assets/_Project/Scripts/LevelManagement/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using CountMasters.Helpers;
+using UnityEngine;
+
+namespace CountMasters.LevelManagement
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private List<Level> levels;
+        private Level _currentLevel;
+        [SerializeField] private Transform levelHolder;
+        [SerializeField] private GameEvent onLevelLoaded;
+
+        private void Start()
+        {
+            LoadLevel();
+        }
+
+        public void LoadLevel()
+        {
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (!levels[i].IsLevelDone)
+                {
+                    _currentLevel = levels[i];
+                    Debug.Log(_currentLevel.name);
+                    break;
+                }
+            }
+
+            Instantiate(_currentLevel, levelHolder);
+            onLevelLoaded.Invoke();
+        }
+
+        public void SetLevelIsDone()
+        {
+            _currentLevel.IsLevelDone = true;
+        }
+    }
+}
