cac97bb60fd9dfef1126460d18d846bfdf9c2867
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-05 17:29:03 +0300

    feat: Add FinishBarSO class

---
+++ Assets/_Project/Scripts/ScriptableObjects/FinishBarSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CountMasters.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "FinishBar")]
+    public class FinishBarSO : ScriptableObject
+    {
+        [SerializeField] private int multiplicationAmount = 2;
+
+        public int MultiplicationAmount
+        {
+            get => multiplicationAmount;
+            set => multiplicationAmount = value;
+        }
+    }
+}
