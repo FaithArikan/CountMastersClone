33abf142bf8ab754c82cc149027f5110988705f2
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-06 18:17:25 +0300

    feat: Add PlayerCollider

---
+++ Assets/_Project/Scripts/Player/PlayerCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CountMasters.Player
+{
+    public class PlayerCollider : MonoBehaviour
+    {
+        private BoxCollider boxCol;
+        
+        private void Awake()
+        {
+            boxCol = GetComponent<BoxCollider>();
+        }
+        
+        public void SetColliderSize()
+        {
+            float rad = 0f;
+            foreach (var cl in GetComponentsInChildren<CapsuleCollider>())
+            {
+                rad += cl.radius;
+            }
+
+            boxCol.size = new Vector3(rad / 2, boxCol.size.y, rad / 2);
+        }
+    }
+}
