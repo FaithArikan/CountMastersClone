184fb0556939b7152dc54f90a91e3e58d5c9f059
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-06 21:21:16 +0300

    feat: Refactor Player

--- Assets/_Project/Scripts/Player/Player.cs
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -7,18 +7,12 @@ namespace CountMasters.Player
 {
     public class Player : MonoBehaviour
     {
-        private BoxCollider boxCol;
         
         [SerializeField] private GameEvent onTotalCloneAmountChanged;
         [SerializeField] private GameEvent onFinishAreaEvent;
 
         public event Action<int> CloneAdditionAction; 
         public event Action<int> CloneMultiplicationAction;
-
-        private void Awake()
-        {
-            boxCol = GetComponent<BoxCollider>();
-        }
         
         private void OnTriggerEnter(Collider col)
         {
@@ -32,7 +26,6 @@ namespace CountMasters.Player
             {
                 switch (gate.GateSo.Features)
                 {
-                    //TODO: boxcolSize
                     case GateFeatures.Addition:
                     {
                         gate.Collider.enabled = false;
@@ -40,7 +33,6 @@ namespace CountMasters.Player
                             gate.RemoveOtherGateCollider();
                         gate.RemoveCanvas();
                         CloneAdditionAction?.Invoke(gate.GateSo.Amount);
-                        boxCol.size = CombineColliderBounds(GetComponentsInChildren<Collider>()).extents * 2;
                         break;
                     }
                     case GateFeatures.Multiplication:
@@ -50,27 +42,11 @@ namespace CountMasters.Player
                             gate.RemoveOtherGateCollider();
                         gate.RemoveCanvas();
                         CloneMultiplicationAction?.Invoke(gate.GateSo.Amount);
-                        boxCol.size = CombineColliderBounds(GetComponentsInChildren<Collider>()).extents * 2;
                         break;
                     }
                 }
                 onTotalCloneAmountChanged.Invoke();
-                foreach (var clone in GetComponentsInChildren<Clone>())
-                {
-                    clone.Init();
-                }
             }
-        }
-        
-        private Bounds CombineColliderBounds(Collider[] colliders)
-        {
-            var bounds = colliders[0].bounds;
-        
-            foreach (var colliderComponent in colliders)
-            {
-                bounds.Encapsulate(colliderComponent.bounds);
-            }
-            return bounds;
         }
     }
 }
