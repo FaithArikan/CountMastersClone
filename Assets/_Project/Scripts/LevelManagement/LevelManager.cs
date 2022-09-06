fed7e0cce79bc7186ffe5217d42b84137418ff42
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-07 01:38:07 +0300

    feat: Add LevelManager InstantiateGameObject and DestroyChildren

--- Assets/_Project/Scripts/LevelManagement/LevelManager.cs
+++ Assets/_Project/Scripts/LevelManagement/LevelManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CountMasters.Helpers;
 using UnityEngine;
 
@@ -9,7 +8,7 @@ namespace CountMasters.LevelManagement
     {
         [SerializeField] private List<Level> levels;
         private Level _currentLevel;
-        [SerializeField] private Transform levelHolder;
+        [SerializeField] private GameObject levelHolder;
         [SerializeField] private GameEvent onLevelLoaded;
 
         private void Start()
@@ -19,6 +18,7 @@ namespace CountMasters.LevelManagement
 
         public void LoadLevel()
         {
+            DestroyChildren(levelHolder.transform);
             for (int i = 0; i < levels.Count; i++)
             {
                 if (!levels[i].IsLevelDone)
@@ -28,13 +28,47 @@ namespace CountMasters.LevelManagement
                 }
             }
 
-            Instantiate(_currentLevel, levelHolder);
+            InstantiateGameObject(levelHolder, _currentLevel);
             onLevelLoaded.Invoke();
         }
 
         public void SetLevelIsDone()
         {
             _currentLevel.IsLevelDone = true;
+        }
+        private GameObject InstantiateGameObject(GameObject parent, Level prefab)
+        {
+            Level level = Instantiate(prefab);
+            if (level != null && parent != null)
+            {
+                Transform t = level.transform;
+                t.SetParent(parent.transform);
+                t.localPosition = Vector3.zero;
+                t.localRotation = Quaternion.identity;
+                t.localScale = Vector3.one;
+                RectTransform rect = level.transform as RectTransform;
+                if (rect != null)
+                {
+                    rect.anchoredPosition = Vector3.zero;
+                    rect.localRotation = Quaternion.identity;
+                    rect.localScale = Vector3.one;
+                    if (rect.anchorMin.x != rect.anchorMax.x && rect.anchorMin.y != rect.anchorMax.y)
+                    {
+                        rect.offsetMin = Vector2.zero;
+                        rect.offsetMax = Vector2.zero;
+                    }
+                }
+                level.gameObject.layer = parent.layer;
+            }
+            return level.gameObject;
+        }
+
+        private void DestroyChildren(Transform t)
+        {
+            foreach (Transform child in t)
+            {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
