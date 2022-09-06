86cce5d8e8d33b1b00ea0429e2b449d73c5d4965
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-06 21:21:35 +0300

    feat: Add OnLevelBegin event to the start popup

--- Assets/_Project/Scripts/Popups/StartPopup.cs
+++ Assets/_Project/Scripts/Popups/StartPopup.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using CountMasters.Helpers;
+using UnityEngine;
 
 namespace CountMasters.Popups
 {
     public class StartPopup : MonoBehaviour
     {
-        
+        [SerializeField] private GameEvent onLevelBegin;
+
+        public void OnLevelBegin()
+        {
+            onLevelBegin.Invoke();
+        }
     }
 }
