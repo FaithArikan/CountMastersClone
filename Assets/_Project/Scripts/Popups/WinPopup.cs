c585cd0b4938d5275c0b276763d2525814a9a28d
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-07 01:16:35 +0300

    refactor: Change WinPopup

--- Assets/_Project/Scripts/Popups/WinPopup.cs
+++ Assets/_Project/Scripts/Popups/WinPopup.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using CountMasters.Helpers;
+using UnityEngine;
 
 namespace CountMasters.Popups
 {
     public class WinPopup : MonoBehaviour
     {
-        
+        [SerializeField] private GameEvent onNextLevelTapped;
+
+        public void OnNextLevel()
+        {
+            onNextLevelTapped.Invoke();
+        }
     }
 }
